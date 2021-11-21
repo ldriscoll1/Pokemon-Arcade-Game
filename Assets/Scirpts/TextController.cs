50b508a2a06864374bf1487599b37f1b06b3d79e
Luke Driscoll <ldriscoll@chapman.edu>
2021-11-20 22:09:07 -0800

    Started Working on fixing the Text Controller

---
+++ Assets/Scirpts/TextController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextController : MonoBehaviour
+{
+    //Enum that specifies what type of Text it is
+    public enum TextType
+    {
+        PokemonName,
+        PokemonLevel,
+        PokemonHealth,
+        EndText
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
